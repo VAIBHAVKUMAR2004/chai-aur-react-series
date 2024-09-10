8e45c5939de0cc342f894543bb4f033ea8e4c897
VAIBHAVKUMAR2004 <vk0101267@gmail.com>
2024-09-10 13:38:16 +0000

    props

--- 03csstailwind/src/components/Card.jsx
+++ 03csstailwind/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function Card(props){
-    console.log("props",props);
+function Card(username,btnText="visit me"){
+    console.log(username);
     
     return(
         <div class="relative h-[400px] w-[300px] rounded-md">
@@ -12,13 +12,13 @@ function Card(props){
   />
   <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
   <div class="absolute bottom-4 left-4 text-left">
-    <h1 class="text-lg font-semibold text-white">Delba</h1>
+    <h1 class="text-lg font-semibold text-white">{username}</h1>
     <p class="mt-2 text-sm text-gray-300">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
       debitis?
     </p>
     <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-      View Profile →
+     {btnText} -
     </button>
   </div>
 </div>
